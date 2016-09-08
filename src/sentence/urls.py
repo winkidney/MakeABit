d769c5a2f8071cb764643743da776ecd8c3f855a
winkidney <winkidney@gmail.com>
2016-09-07 22:05:56 -0700

    Update: Add redirection for /

--- src/sentence/urls.py
+++ src/sentence/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.shortcuts import redirect
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^sentences/', include("words.urls")),
+    url(r'^$', lambda request: redirect("/sentences")),
 ]
